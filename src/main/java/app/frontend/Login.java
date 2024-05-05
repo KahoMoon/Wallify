fbdc9398f8cd156ddb31f7597c16f231a31676ce
KahoMoon <kahomoon1@gmail.com>
2024-05-05 16:35:18 -0700

    Changed page to open explanation page instead of initiate Spotify login.

--- src/main/java/app/frontend/Login.java
+++ src/main/java/app/frontend/Login.java
@@ -1,23 +1,13 @@
 package app.frontend;
 
-import spotify.AuthController;
-
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class Login {
     private JPanel panel1;
     private JPanel accentColor;
-    private JLabel usernameLabel;
-    private JLabel passwordLabel;
-    private JPasswordField passwordText;
-    private JTextField usernameText;
-    private JButton loginButton;
+    private JButton getStartedButton;
 
     public static void main(String[] args) {
         new Login();
@@ -25,21 +15,17 @@ public class Login {
 
     public Login() {
         JFrame jFrame = new JFrame();
+        jFrame.setResizable(false);
         jFrame.setContentPane(panel1);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setTitle("This is a test");
+        jFrame.setTitle("Wallify");
         jFrame.setSize(700, 400);
         jFrame.setVisible(true);
 
-        loginButton.addActionListener(new ActionListener() {
+        getStartedButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                Desktop desk = Desktop.getDesktop();
-                try {
-                    URI uri = new URI(AuthController.spotifyLogin());
-                    desk.browse(uri);
-                } catch (URISyntaxException | IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                new LoginExplanation();
             }
         });
     }
