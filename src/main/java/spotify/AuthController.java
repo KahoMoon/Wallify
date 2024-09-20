c9cbf9d33c9ab1eb8523127ff3cd64191c366d02
KahoMoon <kahomoon1@gmail.com>
2024-09-20 01:18:57 -0700

    Added Javadoc comments.

--- src/main/java/spotify/AuthController.java
+++ src/main/java/spotify/AuthController.java
@@ -1,5 +1,6 @@
 package spotify;
 
+import app.frontend.Login;
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
@@ -8,42 +9,29 @@ import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCrede
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.net.*;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class AuthController {
-    public static void main(String[] args) throws IOException, URISyntaxException {
 
-//        Desktop desk = Desktop.getDesktop();
-//        try {
-//            URI uri = new URI(AuthController.spotifyLogin());
-//            desk.browse(uri);
-//        } catch (URISyntaxException | IOException ex) {
-//            throw new RuntimeException(ex);
-//        }
-
-//        String responseURL = "http://localhost:3000/?code=AQC1cLeVtoz9P2yWwO40XBu1ZLTZXouKUZA0PtKEzYZSDbdL_7gTpOgp7Ho4nljqKzUFXSLXnAGlNqDnHCWRRKC8u3RJ48Jqrn7gelIRvwRvH94b0K53dxr6ztMnur95zqEW8HExKDOrL-nip4SQdFI806yiYz7w7jUjkm_M3tLWj8VbUrBkGyMbSHNiBrR8dWrIH5zjWIkjdUXHVy28PC4Leuun_az_8lBIa2OOrV-aBh7L6mylT-KBJ1QhPsdDbTCssCma7mumtDFV4x_OVjjxO5FA_Lm4ZmVuLiTy7tCJydoFhuAMsbzfXFEEMKrfHzg7fulFcVCI_aIl";
-//        Map<String, String> response = splitQuery(new URI(responseURL).toURL());
-//
-//        System.out.println(getSpotifyUserCode(response.get("code")));
-
-        System.out.println(Arrays.toString(Data.getUserTopArtists()));
-
+    public static void main(String[] args) throws MalformedURLException, UnsupportedEncodingException {
+        System.out.println(splitQuery(new URL(spotifyRedirectUriResponse)));
     }
 
+    /*A hard coded url for post-Spotify-permission acceptance. Use only for development and testing*/
     private static final String spotifyRedirectUriResponse = "http://localhost:3000/?code=AQC7xnSyoPKVq5fhTVCEdSxIM2C8zC0PC7DD7paQTHbJ7n9DiLMn7ylM83bO4KRiPQabf_UVZsiL3oX3SGBYRqvkD0bc4Ghwy0JeyQmufgArVtYGa37vkJSn72TcpZuDzWybvvVjmoyeQ4thbh2dYqjrDUvW8h6Efwv6UyX7uTGMAYruwssLDIRxrCl23IMCwqO47ZpJnb80sp4-nM-VpcubTMiJNAS4yRvYubg5ZYU0_Ax9qVjY_zhmpo8pVLSkpGaVC8_I2pqGUWlvdoBpHHxs1ocA7AqS2AYZZvLtfmMLxUbBqYoQiyVTA6nmwSDnyBYp-9DiRfYTHahs";
 
+    /*URL that the user is redirected to after accepting the permissions (need to change to custom URL protocol)*/
     private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:3000");
     private static String code = "";
 
+    //necessary for Spotify API access
     static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId(Keys.CLIENT_ID.getId())
             .setClientSecret(Keys.CLIENT_SECRET.getSecret())
@@ -53,6 +41,7 @@ public class AuthController {
     private static final AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
             .build();
 
+    /*Generates the url for the respective Spotify permissions*/
     public static String spotifyLogin() {
         AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
                 .scope("playlist-read-private, playlist-read-collaborative, user-follow-read, user-top-read, user-read-recently-played, user-library-read")
@@ -62,6 +51,9 @@ public class AuthController {
         return uri.toString();
     }
 
+    /*Parses the given URL and returns the key value pairs
+    * @param url the url that will be parsed
+    * @return the key value pairs from the url*/
     public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
         Map<String, String> query_pairs = new LinkedHashMap<String, String>();
         String query = url.getQuery();
@@ -73,6 +65,24 @@ public class AuthController {
         return query_pairs;
     }
 
+    /*Checks whether the parsed Spotify response from the login attempt is successful
+    * @param response the parsed response
+    * @return the login status*/
+    public static Boolean checkLogin(Map<String, String> response) {
+        if (response.get("code") != null) {
+            return true;
+        }
+        return false;
+    }
+
+    /*Returns the error code from the unsuccessful login attempted
+    * @param response the parsed response*/
+    public static String getError(Map<String, String> response) {
+        return response.get("error");
+    }
+
+    /*Requests the user's Spotify access token
+    * @return the user access token in the case of a successful request*/
     public static String getSpotifyUserCode(String userCode) {
         code = userCode;
         AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
@@ -83,9 +93,8 @@ public class AuthController {
             spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
             spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
 
-            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn() + " seconds");
         } catch (IOException | ParseException | SpotifyWebApiException e) {
-            //throw new RuntimeException(e);
             System.out.println("Error: " + e.getMessage());
         }
 
