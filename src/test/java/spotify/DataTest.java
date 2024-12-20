f816e4008292d5abfcf0ad27afcf94f311e520e7
KahoMoon <kahomoon1@gmail.com>
2024-12-19 16:27:04 -0800

    Init.

---
+++ src/test/java/spotify/DataTest.java
@@ -0,0 +1,23 @@
+package spotify;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTest {
+
+    @org.junit.jupiter.api.Test
+    void getUserTopArtists() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUsersTopTracks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUsersPlaylists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPlaylistItems() {
+    }
+}
